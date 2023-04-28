92d373ddd2d76dbe69d539f72e36ca285c509c13
dcoldeira <dcoldeira@gmail.com>
2023-04-28 15:46:11 +0100

    updating code as per post III

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,51 @@
-#include <U8g2lib.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BMP085.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <SPI.h>
 #include <WiFiClientSecure.h>
+#include <RTClib.h>
 
 #define BMP085_ADDRESS 0x76
 
 // Replace with your network credentials
-const char* ssid = "xxx";
-const char* password = "xxx";
+const char* ssid = "zzz";
+const char* password = "xxxx";
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define BUTTON_PIN 2
+#define BUZZER_PIN 16
+#define LED_PIN 17
+#define PHOTO_PIN A0
+#define PHOTO_RESISTOR 10000
 
 // Create an instance of the BMP180 sensor
-Adafruit_BMP085 bmp;
+Adafruit_BME280 bme;
 
 // Create an instance of the WebServer
 WebServer server(80);
 
-<<<<<<< HEAD
-// Create an instance of the display
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); 
-=======
-// Create an instance of the OLED display
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
->>>>>>> refs/remotes/origin/main
-
+// Create an instance of the real-time clock
+RTC_DS3231 rtc;
 // Function prototype for handleRoot()
 void handleRoot();
+
+// Function prototype for getFormattedTime()
+String getFormattedTime();
+
+int buttonState = 0;
+int buzzerState = LOW;
+int ledState = LOW;
+int mode = 0;
+int buzzerThreshold = 500;
+int ledThreshold = 2000;
 
 void setup() {
   // Start the serial communication
@@ -46,104 +61,80 @@ void setup() {
   // Print the IP address of the board
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  
-  // Set up the OLED display
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_6x10_tf); 
-  u8g2.clearBuffer();
-  u8g2.setCursor(0, 10);
-  u8g2.print("Hello world!");
-  u8g2.sendBuffer();
-
-  // Initialize the BMP180 sensor
-  if (!bmp.begin(BMP085_ADDRESS)) {
-    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-    while (1);
-  }
-
-<<<<<<< HEAD
-  // Serve the web page\n  server.on("/", handleRoot);
-=======
-  // Initialize the OLED display
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println("SSD1306 allocation failed");
-    for (;;);
-  }
-
-  // Serve the web page
-  server.on("/", handleRoot);
->>>>>>> refs/remotes/origin/main
-  server.begin();
-
-  // Display a message on the OLED screen
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Weather Station");
-  display.println("with ESP32 WROOM");
-  display.println("and BMP180 Sensor");
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
   delay(2000);
   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Starting up...");
+  display.display();
+  if (!bme.begin(0x76)) {
+    display.println("Could not find a valid BMP180 sensor, check wiring!");
+    display.display();
+    while (1);
+  }
+}
+
+String getFormattedTime() {
+  // Get the current time
+  time_t now = time(nullptr);
+
+  // Convert the current time to a struct tm
+  struct tm *timeinfo = localtime(&now);
+
+  // Create a string for the formatted time
+  char formattedTime[20];
+  strftime(formattedTime, sizeof(formattedTime), "%H:%M:%S", timeinfo);
+
+  // Return the formatted time as a string
+  return String(formattedTime);
 }
 
 void loop() {
-  // Handle web requests
-  server.handleClient();
-
-  // Read the temperature and pressure from the BMP180 sensor
-  float temperature = bmp.readTemperature();
-  float pressure = bmp.readPressure() / 100.0F;
-
-  // Display the readings on the OLED screen
+  float temperature = bme.readTemperature();
+  float humidity = bme.readHumidity();
+  float pressure = bme.readPressure() / 100.0F;
+  int lightLevel = analogRead(PHOTO_PIN);
+  int buzzerOn = (pressure > buzzerThreshold || temperature > buzzerThreshold) ? 1 : 0;
+  int ledOn = (pressure > ledThreshold || temperature > ledThreshold) ? 1 : 0;
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == HIGH) {
+    mode = (mode + 1) % 3;
+    delay(500);
+  }
   display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  display.print("Temperature: ");
-  display.print("\n");
-  display.print(temperature, 1);
-  display.println("C");
-  display.setCursor(0, 40);
-  display.print("Pressure: ");
-  display.print("\n");
-  display.print(pressure);
-  display.println("hPa");
-  display.display();
-
-  // Display the readings on the OLED display
-  u8g2.clearBuffer();
-  u8g2.setCursor(0, 10);
-  u8g2.print("Temperature = ");
-  u8g2.print(temperature, 1);
-  u8g2.print(" C");
-  u8g2.setCursor(0, 20);
-  u8g2.print("Pressure = ");
-  u8g2.print(pressure);
-  u8g2.print(" hPa");
-  u8g2.sendBuffer();
-
-  // Wait for a second before taking the next reading
+  display.setCursor(0, 0);
+  switch (mode) {
+    case 0:
+      display.println("Temperature:");
+      display.print(temperature);
+      display.println(" *C");
+      break;
+    case 1:
+      display.println("Humidity:");
+      display.print(humidity);
+      display.println("%");
+      break;
+    case 2:
+      display.println("Pressure:");
+      display.print(pressure);
+      display.println(" hPa");
+      break;
+  }
+  if (buzzerOn == 1) {
+    digitalWrite(BUZZER_PIN, HIGH);
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+  if (ledOn == 1) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
   delay(1000);
 }
-void handleRoot() {
-  // Build the HTML page
-  String html = "<html><body>";
-  html += "<h1 style='text-align:center;'>Weather Station with an ESP32 WROOM 32D and a BMP180 Sensor</h1>";
-  html += "<div style='display:flex; justify-content:center;'>";
-  html += "<div style='margin:20px; text-align:center;'>";
-  html += "<h2>Temperature</h2>";
-  html += "<p style='font-size:48px;'>" + String(bmp.readTemperature(), 1) + " &deg;C</p>";
-  html += "</div>";
-  html += "<div style='margin:20px; text-align:center;'>";
-  html += "<h2>Pressure</h2>";
-  html += "<p style='font-size:48px;'>" + String(bmp.readPressure() / 100.0F) + " hPa</p>";
-  html += "</div>";
-  html += "</div>";
-  html += "</body></html>";
-
-  // Send the HTML page to the client
-  server.send(200, "text/html", html);
-}
-
